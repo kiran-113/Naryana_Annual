c5900a8486a87b1a33b602ea243ba912f5c167c9
harsha <nakirikantikiran@outlook.com>
2024-03-15 07:47:20 +0530

    ddd

---
+++ Note_test.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Hide the startup animation after a delay and redirect to index2.html
+    setTimeout(function() {
+        window.location.href = "index.html"; // Redirect to index2.html
+    }, 20000); // Adjust the delay as needed (in milliseconds)
+});
